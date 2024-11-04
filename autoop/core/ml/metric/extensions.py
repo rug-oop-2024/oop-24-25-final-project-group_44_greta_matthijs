f927177ca5c649fbee18d797fd388b13f5a57103
MatthijsPrinsen <m.g.j.prinsen@student.rug.nl>
2024-11-04 13:41:29 +0100

    Added AccuracyInterval metric extension

--- autoop/core/ml/metric/extensions.py
+++ autoop/core/ml/metric/extensions.py
@@ -34,3 +34,24 @@ class Accuracy(Metric):
             if predictions[i] == ground_truth[i]:
                 count += 1
         return count / n_predictions
+
+
+class AccuracyInterval(Metric):
+    r"""
+    Calculates the accuracy with an acceptance interval of size 2*x.
+
+    $\text{Accuracy} =
+    \frac{1}{n}\sum_{i=1}^{n}\mathbb{I}[\hat{y}^{(i)} in [y^{i}-x, y^{i}+x]]$
+    """
+
+    @property
+    def _evaluate(self, predictions, ground_truth, interval: float = 0) -> float:
+        n_predictions = len(predictions)
+        count = 0
+        for i in range(0, n_predictions):
+            if predictions[i] in range(
+                ground_truth[i] - interval, ground_truth[i] + interval
+            ):
+                count += 1
+
+        return count / n_predictions
