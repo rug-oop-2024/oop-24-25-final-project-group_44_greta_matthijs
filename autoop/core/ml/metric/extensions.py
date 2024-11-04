62d6460dffd8d1dac93ff6970be1d504f0f3d3c0
MatthijsPrinsen <m.g.j.prinsen@student.rug.nl>
2024-11-04 15:25:18 +0100

    Added precision metric extension

--- autoop/core/ml/metric/extensions.py
+++ autoop/core/ml/metric/extensions.py
@@ -55,3 +55,63 @@ class AccuracyInterval(Metric):
                 count += 1
 
         return count / n_predictions
+
+
+class Precision(Metric):
+    """
+    Calculates the precision.
+
+    {positve} should be in ["pos_int", "neg_int", True, False]
+        default is set for True values, positve int
+
+    p = True positive / all positives
+    """
+
+    @property
+    def _evaluate(
+        self, predictions, ground_truth, positive: str | bool | None = None
+    ) -> float:
+        def positives_counter(positive, prediction):
+            if positive is None:
+                if prediction is True or (
+                    isinstance(prediction, (float, int)) and prediction > 0
+                ):
+                    return True
+
+            options = ["pos_int", "neg_int", True, False]
+            if positive not in options:
+                raise TypeError("Invalid argument for 'positive'.")
+
+            if positive == "pos_int":
+                if prediction > 0:
+                    return True
+                return False
+
+            elif positive == "neg_int":
+                if prediction < 0:
+                    return True
+                return False
+
+            elif positive is True:
+                if prediction is True:
+                    return True
+                return False
+
+            elif positive is False:
+                if prediction is False:
+                    return True
+                return False
+
+        true_positives = int(0)
+        all_positives = int(0)
+        n_predictions = len(predictions)
+        for i in range(0, n_predictions):
+            if predictions[i] == ground_truth[i]:
+                true_positives += 1
+            if positives_counter(positive, predictions[i]):
+                all_positives += 1
+
+        if true_positives == 0 or all_positives == 0:
+            return 0
+
+        return true_positives / all_positives
